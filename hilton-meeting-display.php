f92332b8a02b041be6d382037816896a208c379b
terratec2k4 <iphonjavi@gmail.com>
2026-07-17 17:40:22 +0200

    Add files via upload
    beta 2

--- hilton-meeting-display.php
+++ hilton-meeting-display.php
@@ -1,24 +1,49 @@
 <?php
 /**
  * Plugin Name: Hilton Meeting Display
+ * Plugin URI: https://github.com/terratec2k4/hilton-meeting-display
  * Description: Professional Meeting Room Display System.
- * Version: 2.0.0-alpha1
+ * Version: 2.0.0-alpha2
  * Requires at least: 6.5
  * Requires PHP: 8.2
  * Author: Javier Mesa
  * Text Domain: hilton-meeting-display
+ * Domain Path: /languages
  */
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'HMD_VERSION', '2.0.0-alpha1' );
+/*
+|--------------------------------------------------------------------------
+| Plugin Constants
+|--------------------------------------------------------------------------
+*/
+
+define( 'HMD_VERSION', '2.0.0-alpha2' );
 define( 'HMD_PLUGIN_FILE', __FILE__ );
 define( 'HMD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'HMD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
+/*
+|--------------------------------------------------------------------------
+| Core Classes
+|--------------------------------------------------------------------------
+*/
+
 require_once HMD_PLUGIN_DIR . 'includes/class-hmd-database.php';
+
+require_once HMD_PLUGIN_DIR . 'includes/class-hmd-room-repository.php';
+require_once HMD_PLUGIN_DIR . 'includes/class-hmd-room-controller.php';
+require_once HMD_PLUGIN_DIR . 'includes/class-hmd-room-list-table.php';
+
 require_once HMD_PLUGIN_DIR . 'includes/class-hmd-admin.php';
 require_once HMD_PLUGIN_DIR . 'includes/class-hmd-plugin.php';
+
+/*
+|--------------------------------------------------------------------------
+| Activation / Deactivation
+|--------------------------------------------------------------------------
+*/
 
 register_activation_hook(
     __FILE__,
@@ -29,5 +54,11 @@ register_deactivation_hook(
     __FILE__,
     array( 'HMD_Plugin', 'deactivate' )
 );
+
+/*
+|--------------------------------------------------------------------------
+| Bootstrap
+|--------------------------------------------------------------------------
+*/
 
 HMD_Plugin::instance();
